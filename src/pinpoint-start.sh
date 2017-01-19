b4fe26151d41bb8368881f383478524c32823f2b
Marcos Entenza <mak@redhat.com>
2017-01-19 14:35:01 +0000

    Moify entrypoint script

--- src/pinpoint-start.sh
+++ src/pinpoint-start.sh
@@ -5,3 +5,7 @@
 /pinpoint/quickstart/bin/start-hbase.sh &> /pinpoint/logs/hbase.out
 /pinpoint/quickstart/bin/start-collector.sh &> /pinpoint/logs/collector.out &
 /pinpoint/quickstart/bin/start-web.sh &> /pinpoint/logs/webui.out
+
+# Comment out the following line if you want Test App to automatically be started
+
+#/pinpoint/quickstart/bin/start-testapp.sh &> /pinpoint/logs/testapp.out &
