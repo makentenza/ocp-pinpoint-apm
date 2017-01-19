308e30f0f31e7715ed54e981e8a3210d3ee92cbb
Marcos Entenza <mak@redhat.com>
2017-01-19 14:31:13 +0000

    Modify entrypoint script

--- src/pinpoint-start.sh
+++ src/pinpoint-start.sh
@@ -5,4 +5,3 @@
 /pinpoint/quickstart/bin/start-hbase.sh &> /pinpoint/logs/hbase.out
 /pinpoint/quickstart/bin/start-collector.sh &> /pinpoint/logs/collector.out &
 /pinpoint/quickstart/bin/start-web.sh &> /pinpoint/logs/webui.out
-/pinpoint/quickstart/bin/start-testapp.sh &> /pinpoint/logs/testapp.out &
